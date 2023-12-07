4e3c4f4e6213f1b01ef558ff70cad7ceeb1dabe8
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-07 19:47:48 +0530

    wip

---
+++ scripts/beforeinit.js
@@ -0,0 +1,3 @@
+// Fetch settings from GitHub
+var url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/dev/config/settings.yaml";
+resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
