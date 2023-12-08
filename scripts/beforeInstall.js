6ac77b6f7c564d257860ce14f55c61f3dbbf979b
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-08 13:08:23 +0530

    wip

---
+++ scripts/beforeInstall.js
@@ -0,0 +1,31 @@
+var result = {
+    result: 0,
+    nodes: []
+};
+
+if ('${settings.dbType:standalone}' == 'cluster') {
+    resp.nodes.push({
+        nodeType: "postgresql",
+        count: 2,
+        cloudlets: 32,
+        diskLimit: `${settings.dbDiskLimit:10}`,
+        scalingMode: "STATELESS",
+        isSLBAccessEnabled: true,
+        nodeGroup: "sqldb",
+        password: `${globals.dbPassword}`,
+        cluster: {
+            is_pgpool2: true,
+        }
+    })
+} else{
+    resp.nodes.push({
+        nodeType: "postgresql",
+        count: 1,
+        cloudlets: 32,
+        diskLimit: `${settings.dbDiskLimit:10}`,
+        scalingMode: "STATEFUL",
+        isSLBAccessEnabled: true,
+        nodeGroup: "sqldb",
+        password: `${globals.dbPassword}`,
+    })
+}
