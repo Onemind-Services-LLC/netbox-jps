c6b0d002c575bb65e9b9cd4f87a967f899a64b23
gauravonemind <120097255+gauravonemind@users.noreply.github.com>
2023-12-08 07:47:30 +0530

    Update create_database.py
    Signed-off-by: gauravonemind <120097255+gauravonemind@users.noreply.github.com>

--- create_database.py
+++ create_database.py
@@ -2,10 +2,8 @@ import psycopg2
 from psycopg2 import sql
 import os
 
-def create_postgres_database(host, port, user, password, new_database):
-    connection = None
-    cursor = None
 
+def create_postgres_database(host, port, user, password, database_name):
     try:
         # Connect to the PostgreSQL server
         connection = psycopg2.connect(
@@ -13,28 +11,36 @@ def create_postgres_database(host, port, user, password, new_database):
             port=port,
             user=user,
             password=password
+            database='postgres'
         )
 
         # Create a cursor object to interact with the database
         cursor = connection.cursor()
 
         # Execute a query to create the new database
-        create_database_query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(new_database))
+        create_database_query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(database_name))
+        
+        # Ensure that the CREATE DATABASE statement is not within a transaction block
+        connection.autocommit = True
+
         cursor.execute(create_database_query)
 
-        print(f"Database '{new_database}' created successfully.")
+        print(f"Database '{database_name}' created successfully.")
 
     except Exception as e:
         print(f"Error: {e}")
 
     finally:
+        # Reset autocommit to its default value
+        if connection:
+            connection.autocommit = False
+
         # Close the cursor and connection in the finally block
         if cursor:
             cursor.close()
         if connection:
             connection.close()
 
-# Replace the following with your PostgreSQL credentials and the new database name
 
 host = os.environ.get('POSTGRES_HOST', 'Unknown Host')
 port = 5432
@@ -42,6 +48,4 @@ user = os.environ.get('DB_USER', 'webadmin')
 password = os.environ.get('DB_PASSWORD', 'webadmin')
 database_name = os.environ.get('DB_NAME', 'webadmin')
 
-
-# Call the function with your credentials
 create_postgres_database(host, port, user, password, database_name)
