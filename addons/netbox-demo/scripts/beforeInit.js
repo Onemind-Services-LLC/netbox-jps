397616201386634114f30897bf6114f41d2cc469
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-12 01:31:22 +0530

    adds dynamic baseUrl to scripts

--- addons/netbox-demo/scripts/beforeInit.js
+++ addons/netbox-demo/scripts/beforeInit.js
@@ -1,5 +1,5 @@
 // Fetch settings from GitHub
-var resp = {result:0};
-var url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/master/addons/netbox-demo/config/settings.yaml";
+var resp = {result: 0};
+var url = jps.baseUrl + "/addons/netbox-demo/config/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 return resp;
