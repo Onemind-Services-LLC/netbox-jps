8bbc329e106b27005509430347f14f3e56ca54d8
gauravonemind <120097255+gauravonemind@users.noreply.github.com>
2023-12-11 18:23:38 +0530

    version value dynamic added.

--- addons/netbox-secrets/scripts/beforeInit.js
+++ addons/netbox-secrets/scripts/beforeInit.js
@@ -3,16 +3,9 @@ var resp = {result:0};
 var url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/gaurav_dev/addons/netbox-secrets/config/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 
-const version_values = [
-    {
-        value: 'v1.7.6',
-        caption: 'v1.7.6'
-    },
-    {
-        value: 'v1.7.7',
-        caption: 'v1.7.7'
-    }
-]
+version_file_url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/gaurav_dev/addons/netbox-secrets/config/versions.yaml";
+
+const version_values = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(version_file_url)))
 
 version_field = {
     name: 'version',
@@ -21,7 +14,6 @@ version_field = {
     tooltip: 'Version of plugin to install',
     values: version_values,
     default: 'v1.7.6'
-    
 }
 
 
