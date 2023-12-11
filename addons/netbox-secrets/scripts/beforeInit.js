f87c71d6f53ac0ed4e407221e0baac2416f50940
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-12 01:10:48 +0530

    adds versions.yaml fetch via API

--- addons/netbox-secrets/scripts/beforeInit.js
+++ addons/netbox-secrets/scripts/beforeInit.js
@@ -1,14 +1,14 @@
 // Fetch settings from GitHub
 let resp = {result: 0};
 
-const url = "${jps.baseUrl}/addons/netbox-secrets/config/settings.yaml";
-const version_file_url = "${jps.baseUrl}/addons/netbox-secrets/config/versions.yaml";
+const url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/gaurav_dev/addons/netbox-secrets/config/settings.yaml";
+const version_file_url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/gaurav_dev/addons/netbox-secrets/config/versions.yaml";
 
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 
 const versions = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(version_file_url)))
 
-resp.settings.fields.push({
+resp.settings.fields.unshift({
     name: 'version',
     type: 'list',
     caption: 'Version',
