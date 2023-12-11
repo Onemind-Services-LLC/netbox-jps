7b72b0bc49e7ae719dbd9e4d7373515f33033e3e
gauravonemind <120097255+gauravonemind@users.noreply.github.com>
2023-12-11 17:57:38 +0530

    fields added.

--- addons/netbox-secrets/scripts/beforeInit.js
+++ addons/netbox-secrets/scripts/beforeInit.js
@@ -3,6 +3,20 @@ var resp = {result:0};
 var url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/gaurav_dev/addons/netbox-secrets/config/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 
+version_field = {
+    name: 'version',
+    type: 'list',
+    caption: 'Version',
+    tooltip: 'Version of plugin to install',
+    values: [
+       { 
+            value: 'v1.7.6',
+            caption: 'v1.7.6'
+        }
+    ],
+    default: 'v1.7.6'
+}
+
 const more_fields = [
     {
         value: 'v1.7.6',
@@ -13,8 +27,5 @@ const more_fields = [
         caption: 'v1.7.7'
     }
 ]
-
-
-resp.settings.fields = more_fields
 
 return resp;
