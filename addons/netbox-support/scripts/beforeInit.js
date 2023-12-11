12f7d2596000b4640e3f37105c8d916a4efdc5d1
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-11 05:39:39 +0530

    finalize for master branch

--- addons/netbox-support/scripts/beforeInit.js
+++ addons/netbox-support/scripts/beforeInit.js
@@ -1,5 +1,5 @@
 // Fetch settings from GitHub
 var resp = {result:0};
-var url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/dev/addons/netbox-support/config/settings.yaml";
+var url = "https://raw.githubusercontent.com/Onemind-Services-LLC/netbox-jps/master/addons/netbox-support/config/settings.yaml";
 resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
 return resp;
