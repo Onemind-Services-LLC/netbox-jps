9fdd7d00d87e496a33b60f39f8cb22cc578278b1
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-12 18:03:03 +0530

    moved settings for run button

--- addons/netbox-initializers/scripts/beforeInit.js
+++ addons/netbox-initializers/scripts/beforeInit.js
@@ -9,7 +9,7 @@ resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.c
 
 const versions = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(version_file_url)))
 
-resp.settings.fields.unshift({
+resp.settings.fields.main.unshift({
     name: 'version',
     type: 'list',
     caption: 'Version',
