cebbaa27ab0959fb2612ddaed09be28098ae5a15
Abhimanyu Saharan <asaharan@onemindservices.com>
2023-12-27 20:19:17 +0530

    added pypi files to plugins

--- addons/netbox-backups/scripts/beforeInit.js
+++
@@ -1,5 +0,0 @@
-// Fetch settings from GitHub
-var resp = {result: 0};
-var url = jps.baseUrl + "/addons/netbox-backups/config/settings.yaml";
-resp.settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
-return resp;
